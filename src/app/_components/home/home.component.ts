b6d1efe0c504e5a2f2ff23c004293b8d7988b1eb
Eliad Moosavi <iliadm@ca.ibm.com>
2017-07-19 12:58:50 -0400

    Users can stop jobs, start them and complete

--- src/app/_components/home/home.component.ts
+++ src/app/_components/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit }        from "@angular/core";
 import { DatabaseService }          from '../../_services/database/database.service';
 
-import { Observable } from 'rxjs/Observable';
+import { Http, Response }                     from "@angular/http";
+
+import { Observable }               from 'rxjs/Observable';
 
 @Component({
     selector: "em-home",
@@ -13,7 +15,7 @@ export class HomeComponent implements OnInit {
     jobsData: any;
     online: Observable<boolean>;
 
-    constructor (private databaseService: DatabaseService) {
+    constructor (private databaseService: DatabaseService, private http: Http) {
         this.online = databaseService.internetConnection;
     }
 
@@ -44,5 +46,54 @@ export class HomeComponent implements OnInit {
         });
         
         console.log("jobsData", this.jobsData);
+    }
+
+    updateStatus(index, name, status, completed) {
+        this.jobsData[index]["value"]["status"] = status;
+        this.jobsData[index]["value"]["inProgress"] = status === "in-progress";
+
+        this.http.post(`https://polymer-1st-try.firebaseio.com/jobs/${name}/.json`, {
+            "status":       status,
+            "inProgress":   status === "in-progress",
+            "completed":    completed
+        })
+            .map((res: Response) => {
+                if (res && res.status === 200) {
+                    console.log(res.json());
+                }
+            })
+            .catch((error: any) => void {
+                if (error) {
+                    console.error(error);
+                }
+            })
+            .subscribe();
+    }
+
+    startJob(index) {        
+        console.log("You clicked START", index);
+
+        this.updateStatus(index, this.jobsData[index]["name"], "in-progress", false);
+    }
+
+    stopJob(index) {
+        console.log("You clicked STOP", index);
+
+        this.jobsData[index]["stopClicked"] = true;
+    }
+
+    stopOnly(index) {
+        console.log("You clicked STOPONLY");
+
+        this.updateStatus(index, this.jobsData[index]["name"], "approved", false);
+        this.jobsData[index]["stopClicked"] = false;
+    }
+
+    completeJob(index) {
+        console.log("You clicked COMPLETEJOB");
+
+        this.updateStatus(index, this.jobsData[index]["name"], "completed", true);
+        
+        this.jobsData[index]["stopClicked"] = false;
     }
 }
